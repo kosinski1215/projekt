c0e922bb3eee94364c116001af7a99f23d1b7ce2
kosinski1215 <nanometr2001@gmail.com>
2015-03-28 15:23:59 +0100

    s

---
+++ projekt/src/pl/kosinski/Projekt.java
@@ -0,0 +1,20 @@
+package pl.kosinski;
+
+
+public class Projekt {
+	
+	
+	public static void main(String[] args) {
+		Projekt projekt = new Projekt();
+		projekt.print("banan");
+		
+		
+	}
+	
+	public void print(String tekst) {
+		System.out.println(tekst);
+		
+	}
+	
+	
+}
